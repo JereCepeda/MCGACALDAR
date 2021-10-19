bd8456effcf874e2b66fc1e34093c6cf2d2b72ac
lucasboasso <lucassebastian.boasso@alumnos.uai.edu.ar>
2021-10-19 19:20:37 -0300

    Cambie el bodyparser

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,9 +1,8 @@
 const express = require ('express')
-const bodyParser = require('body-parser')
 const controller = require ('../../controllers/users/controller')
 
 const rout = express.Router();
-const jsonParser = bodyParser.json()
+const jsonParser = express.json()
 rout.get('/showall_user',jsonParser,controller.indexClients);
 rout.post('/addUser',jsonParser, controller.addUser);
 rout.get('/:_id',jsonParser,controller.getUserById);
