7c474bf9407e5e85cc49f122c8a243c9aae80b62
lucasboasso <lucassebastian.boasso@alumnos.uai.edu.ar>
2021-10-19 21:03:36 -0300

    Correccion de ruta ROL

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -4,13 +4,14 @@ const controller = require ('../../controllers/users/controller')
 const rout = express.Router();
 const jsonParser = express.json()
 rout.get('/showall_user',jsonParser,controller.indexClients);
+rout.get('/showall_rol',jsonParser,controller.indexRol);
 rout.post('/addUser',jsonParser, controller.addUser);
 rout.get('/:_id',jsonParser,controller.getUserById);
 rout.delete('/:_id',jsonParser,controller.deleteUser);
 rout.put('/:_id',jsonParser,controller.editUser);
 
 rout.post('/addrol',jsonParser, controller.addRol);
-rout.get('/showall_rol',jsonParser,controller.indexRol);
+
 
 
 
